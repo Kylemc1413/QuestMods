d6e101710df42387eafe13f1210c00ab33344b36
Kyle1413 <kylet1412@gmail.com>
2019-10-11 18:15:24 -0400

    Version

--- MappingExtensions/main.cpp
+++ MappingExtensions/main.cpp
@@ -35,7 +35,7 @@
 #define standard_level_detail_view_refresh_content_offset 0x9EEF38
 #define obstacle_execution_ratings_offset 0x9D1658
 #define MOD_ID "MappingExtenions"
-#define VERSION "0.13.0"
+#define VERSION "0.14.0"
 
 using il2cpp_utils::GetClassFromName;
 using TYPEDEFS_H::Quaternion;
